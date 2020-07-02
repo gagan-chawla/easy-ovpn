b59d92a411c3314a843e9b550400da219aa4c4eb
Gagan Chawla <gagan.chawla@joshtechnologygroup.com>
2020-07-02 23:51:45 +0530

    renamed sourced script variables

--- main.sh
+++ main.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
-FILE=$(find $DIR -maxdepth 1 -name '*.ovpn' | head -1)
-
+EASY_OVPN_DIR=$(dirname $0)
+EASY_OVPN_FILE=$(find $EASY_OVPN_DIR -maxdepth 1 -name '*.ovpn' | head -1)
 easyovpn () {
-	local auth_user_pass=$(openssl enc -base64 -d < $DIR/creds/userpass.txt)
-	local askpass=$(< $DIR/creds/passphrase.txt)
-	local auth_code=$(python $DIR/oath/2fa.py $DIR)
-	sudo openvpn --config $FILE --auth-user-pass =(echo -ne "$auth_user_pass$auth_code") --askpass =(echo -ne "$askpass")
+	local auth_user_pass=$(openssl enc -base64 -d < $EASY_OVPN_DIR/creds/userpass.txt)
+	local askpass=$(< $EASY_OVPN_DIR/creds/passphrase.txt)
+	local auth_code=$(python $EASY_OVPN_DIR/oath/2fa.py $EASY_OVPN_DIR)
+	sudo openvpn --config $EASY_OVPN_FILE --auth-user-pass =(echo -ne "$auth_user_pass$auth_code") --askpass =(echo -ne "$askpass")
 }
